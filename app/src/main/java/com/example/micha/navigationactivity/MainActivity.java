f9850b3146bc175437e8d1e4364e87f546cbab8f
Mazogu <michael_doom@hotmail.com>
2018-01-15 14:44:38 -0500

    Added an icon and overflow to menu

--- app/src/main/java/com/example/micha/navigationactivity/MainActivity.java
+++ app/src/main/java/com/example/micha/navigationactivity/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -104,5 +106,12 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_items, menu);
+        return true;
     }
 }
