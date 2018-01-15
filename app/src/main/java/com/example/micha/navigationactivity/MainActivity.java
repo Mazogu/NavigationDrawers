8a2ebd7474b066ff713bd98e64531357bbd14cfe
Mazogu <michael_doom@hotmail.com>
2018-01-14 23:41:02 -0500

    Navigation bar now pops out

--- app/src/main/java/com/example/micha/navigationactivity/MainActivity.java
+++ app/src/main/java/com/example/micha/navigationactivity/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.micha.navigationactivity;
 
+import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private ListView list;
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        //toggle.syncState();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = findViewById(R.id.drawerLayout);
+        list = findViewById(R.id.drawer);
+        addToDrawer();
+        //toggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open,R.string.drawer_closed);
+        //drawer.addDrawerListener(toggle);
+    }
+
+    private void addToDrawer() {
+        String[] names = getResources().getStringArray(R.array.activity_names);
+        ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, names);
+        list.setAdapter(nameAdapter);
     }
 }
---
+++ app/src/main/java/com/example/micha/navigationactivity/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.example.micha.navigationactivity.utils;
+
+import com.example.micha.navigationactivity.R;
+
+/**
+ * Created by micha on 1/14/2018.
+ */
+
+public class Constants {
+    public static class StringIDs{
+        public static final int MAINID = R.string.Main;
+        public static final int CAMERAID = R.string.Camera;
+        public static final int MEDIAID = R.string.Media;
+        public static final int WEBID = R.string.Web;
+        public static final int POWER = R.string.Power;
+    }
+}
